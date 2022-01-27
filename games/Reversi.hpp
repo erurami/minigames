1e586ee6e1cdeb4bc0bcc8acbed9d1f873bf596f
erurami <88187787+erurami@users.noreply.github.com>
2022-01-27 20:44:57 +0700

    Reversi : add some features to add

--- games/Reversi.hpp
+++ games/Reversi.hpp
@@ -12,8 +12,31 @@ class Game
         explicit Game(int width = 8, int height = 8);
         ~Game();
 
+        int GetBoardWidth(void)  {return m_width;};
+        int GetBoardHeight(void) {return m_height;};
+        int GetWhichTurn(void);
+        // return : total placable position count
+        int GetPlacablePositions(int*** pPlacePositionBufAddr);
+
+        int PutDisc(int x, int y);
+
+        void Undo(void);
+
+        void Print(void);
+
 
     private:
+
+        int** m_pBoard;
+
+        int m_width;
+        int m_height;
+
+        int   m_placablePosCount;
+        int** m_pPlacablePositions;
+
+        int m_turnPlayer;
+        int m_turnNumber;
 
 };
 
