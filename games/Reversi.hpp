f93e4ecb50ea7d44c95e8ceed31a1b18da603068
erurami <88187787+erurami@users.noreply.github.com>
2022-01-27 20:14:17 +0700

    Reversi : add header file

---
+++ games/Reversi.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+
+namespace Reversi
+{
+
+class Game
+{
+
+    public:
+
+        explicit Game(int width = 8, int height = 8);
+        ~Game();
+
+
+    private:
+
+};
+
+}
+
+#include "Reversi.ipp"
+
