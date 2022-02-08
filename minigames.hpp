4a291cce289f97e23615d767714b57d73eb1f7f3
erurami <88187787+erurami@users.noreply.github.com>
2022-02-08 16:53:10 +0700

    Add header file to include when use.

---
+++ minigames.hpp
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#if __has_include("games/4InARow.hpp")
+#include "games/4InARow.hpp"
+#endif
+
+#if __has_include("games/Reversi.hpp")
+#include "games/Reversi.hpp"
+#endif
+
+#if __has_include("games/2048.hpp")
+#include "games/2048.hpp"
+#endif
+
+
+
